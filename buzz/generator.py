4927caa47398d2011ca2f06bb584c31041765351
yassinetahiri <yassinetahiri93@gmail.com>
2020-04-13 12:31:07 +0200

    Adding new value

--- buzz/generator.py
+++ buzz/generator.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('new value', 'continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
